358907b25e76540e900416332e5a9adc0c3a8dbe
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2024-11-21 21:39:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -200,11 +200,11 @@ m.react("🦋")
 //AutoReact 
 if (!isReact && senderNumber !== botNumber) {
 if (config.AUTO_REACT === 'false')
-    m.react("💖");
+    m.react(".");
 }
 if (!isReact && senderNumber === botNumber) {
 if (config.OWNER_REACT === 'false')
-    m.react("😁");
+    m.react(".");
 }                
 //============================        
         
