7179936c99979a9ab27ddd252f251c4894eace5c
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2024-12-16 21:48:46 +0300

    Delete Functions directory

--- Functions/gcPresence.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = async (client, m, gcpresence) => {
-    if (m.isGroup && gcpresence === 'true') {
-        let dreadrecordin = ['recording', 'cheating', '' 'composing'];
-        let dreadrecordinfinal = dreadrecordin[Math.floor(Math.random() * dreadrecordin.length)];
-        await client.sendPresenceUpdate(dreadrecordinfinal, m.chat);
-    }
-};
