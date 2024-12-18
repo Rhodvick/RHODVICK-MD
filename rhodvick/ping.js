dc9a2a0167fff261c1eadc14aeac4c8842161dc5
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2024-12-18 15:44:59 +0300

    Update ping.js

--- rhodvick/ping.js
+++ rhodvick/ping.js
@@ -4,12 +4,18 @@ const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '*🌍RHODVICK 𝒊𝒔 𝒐𝒏𝒍𝒊𝒏𝒆🌍* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
-    let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔 hhyy✨';
+    let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
-    var mp4 = 'https://files.catbox.moe/j8x0so.mp4';
+    var mp4 = 'https://i.ibb.co/p/74421a3c5d94ac0a.jpg.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
 console.log("mon test");
 module.exports = async (context) => {
-      
+        const { client, m, spacespeed } = context;
+
+
+await m.reply(`Pong\n${spacespeed.toFixed(4)}ms`)
+
+}
+
